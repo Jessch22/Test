48b80fb127def9baa6109c57ec17d94c9b2155ce
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-11-20 22:52:59 +0700

    ubah style navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,31 @@
-import "../styles/navbarStyle.css"
+import "../styles/navbarStyle.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faAddressCard, faFolderOpen, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const navigation = [
-  { name: 'Home', href:"#home", current: true},
-  { name: 'About', href:"#about", current: false},
-  { name: 'Projects', href:"#projects", current: false},
-  { name: 'Contact', href:"#contact", current: false},
+  { name: 'Home', href: "#home", logo: faHouse, current: true },
+  { name: 'About', href: "#about", logo: faAddressCard, current: false },
+  { name: 'Projects', href: "#projects", logo: faFolderOpen, current: false },
+  { name: 'Contact', href: "#contact", logo: faEnvelope, current: false },
 ];
 
 const Navbar = () => {
-  return(
-    <>
+  return (
     <div className="navbar">
       <nav>
         {navigation.map((item) => (
           <a
-            key = {item.name}
-            href = {item.href}
-          >{item.name}</a>
+            key={item.name}
+            href={item.href}
+            className={item.current ? 'active' : ''}
+          >
+            <FontAwesomeIcon icon={item.logo} />
+            <span className="nav-text">{item.name}</span>
+          </a>
         ))}
       </nav>
     </div>
-    
-    </>
-  )
-}
+  );
+};
+
 export default Navbar
